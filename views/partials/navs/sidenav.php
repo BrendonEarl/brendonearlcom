4573df3d8f7929f263dfa42b856d6b2d8de662e7
Brendon Earl <brendon@tireguru.net>
2023-07-26 13:44:02 -0600

    resume

--- views/partials/navs/sidenav.php
+++ views/partials/navs/sidenav.php
@@ -5,8 +5,16 @@
             <ul class="list-reset flex flex-row md:flex-col pt-3 md:py-3 px-1 md:px-2 text-center md:text-left">
                 <li class="mr-3 flex-1">
                     <a href="#"
+                       class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-yellow-500">
+                        <i class="fa fa-folder pr-0 md:pr-3"></i><span
+                                class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Home</span>
+                    </a>
+                </li>
+
+                <li class="mr-3 flex-1">
+                    <a href="#"
                        class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
-                        <i class="fas fa-tasks pr-0 md:pr-3"></i><span
+                        <i class="fa fa-address-card pr-0 md:pr-3"></i><span
                             class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">About</span>
                     </a>
                 </li>
@@ -14,7 +22,7 @@
                 <li class="mr-3 flex-1">
                     <a href="#"
                        class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-blue-600">
-                        <i class="fas fa-chart-area pr-0 md:pr-3 text-blue-600"></i><span
+                        <i class="fa fa-keyboard pr-0 md:pr-3 text-blue-600"></i><span
                                 class="pb-1 md:pb-0 text-xs md:text-base text-white md:text-white block md:inline-block">Skills</span>
                     </a>
                 </li>
@@ -22,7 +30,7 @@
                 <li class="mr-3 flex-1">
                     <a href="#"
                        class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-purple-500">
-                        <i class="fa fa-envelope pr-0 md:pr-3"></i><span
+                        <i class="fas fa-tasks pr-0 md:pr-3"></i><span
                             class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Work History</span>
                     </a>
                 </li>
@@ -30,13 +38,11 @@
                 <li class="mr-3 flex-1">
                     <a href="#"
                        class="block py-1 md:py-3 pl-0 md:pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-red-500">
-                        <i class="fa fa-wallet pr-0 md:pr-3"></i><span
-                            class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Download CV</span>
+                        <i class="fa fa-file pr-0 md:pr-4"></i><span
+                            class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">CV</span>
                     </a>
                 </li>
             </ul>
         </div>
-
-
     </div>
 </nav>
